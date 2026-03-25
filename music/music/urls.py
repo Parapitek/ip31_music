d8e7d9ffe5d83692d6b0c24ea68f2ddc3a6a42d4
Rustam Safarov <r.safarov@corp.nstu.ru>
2026-03-25 14:30:03 +0700

    добавил комментарий в urls.py

--- music/music/urls.py
+++ music/music/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from main import views
+# тестовый комментарий
 
 urlpatterns = [
     path('admin/', admin.site.urls),
